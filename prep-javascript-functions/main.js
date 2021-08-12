926b3435bfb7e83c8ec2d90e07c65d6544ce303a
Greggory Garceau <ggarceau212@gmail.com>
2021-08-11 21:49:17 -0400

    added hours to minute function

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -2,5 +2,13 @@ function addTwoNumbers(num1, num2) {
   return num1 + num2;
 }
 
-var sum = addTwoNumbers(4, 4);
-console.log('sum', sum);
+var sum = addTwoNumbers(2, 2);
+console.log('Sum: ', sum);
+
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var minutes = convertHoursToMinutes(2);
+console.log('Minutes : ', minutes);
