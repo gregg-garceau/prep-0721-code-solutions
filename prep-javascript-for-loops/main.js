6464d11037ab4d4ee2fc1e214354d19a6baed087
Greggory Garceau <ggarceau212@gmail.com>
2021-08-11 23:05:45 -0400

    Completed prep javascript for loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+for(let i = 0; i < 10; i++ ) {
+  console.log(i);
+}
+
+
+for(let k = 0; k < 20; k += 2) {
+  console.log(k);
+}
+
+
+for(let m = 100; m > 0; m -= 1) {
+  console.log('Time till explosion: ' + m + '!');
+}
